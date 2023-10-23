548a4f74ffe4a554f2f75b16a755bdeb0dbf8082
zombieMaybeLikeCake <possibilum@gmail.com>
2023-10-23 00:38:06 -0700

    scorend Commit

---
+++ smartCalligraphyPractice/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  smartCalligraphyPractice
+//
+//  Created by 羅琮棠 on 2023/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
